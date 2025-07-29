bc68723020930414e19b3917e0b4183c9c63e202
denny saputra <detrastudios@gmail.com>
2025-07-29 07:48:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.label c

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,26 +3,13 @@ import {
   Facebook,
   Youtube,
   Twitter,
+  AtSign,
   type LucideProps,
 } from 'lucide-react';
 import type { SocialPlatform } from '@/lib/types';
 
 export const ThreadsIcon = (props: LucideProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M12 7.5a5.5 5.5 0 1 0 5.5 5.5" />
-    <path d="M12 7.5A5.5 5.5 0 1 0 6.5 13" />
-  </svg>
+  <AtSign {...props} />
 );
 
 export const TikTokIcon = (props: LucideProps) => (
